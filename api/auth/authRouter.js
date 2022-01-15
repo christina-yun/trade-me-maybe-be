0af40264e9ede314a5ea640bb1f955c7821a860c
Christina Yun <poncheeyun@yahoo.com>
2022-01-15 11:06:19 -0800

    auth register endpoint with middleware is working

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,30 +1,37 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const Users = require('../users/users_model')
+const Users = require("../users/users_model");
 
 // middleware goes here
-
-
+const {
+    restricted,
+    checkUserExists,
+    validateUsername,
+    validatePassword,
+    checkPasswordCorrect,
+    validateContact,
+    hashThePassword
+} = require('../middleware/authMiddleware')
 //[POST] /auth/register
-router.post('/register', (req, res, next) => {
-    Users.create(req.body)
+router.post("/register", validateUsername, validatePassword, validateContact, hashThePassword, (req, res, next) => {
+  Users.create(req.body)
     .then((newUser) => {
-        res.status(201).json(newUser)
+      res.status(201).json(newUser);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 //[POST] /auth/login
-router.post('/login', (req, res, next) => {
-    try{
-        res.status(200).json({
-            user_id: req.user_id,
-            message: `Welcome back ${req.body.username}`,
-            token: req.token
-        })
-    } catch(err){
-        next(err)
-    }
-})
+router.post("/login", (req, res, next) => {
+  try {
+    res.status(200).json({
+      user_id: req.user_id,
+      message: `Welcome back ${req.body.username}`,
+      token: req.token,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = router;
