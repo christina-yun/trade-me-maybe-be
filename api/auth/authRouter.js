34fcf1a55f41f08136b55cbb37c3a41cccf88477
Christina Yun <poncheeyun@yahoo.com>
2022-01-15 11:41:31 -0800

    cleaned up authRouter

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -3,35 +3,46 @@ const router = express.Router();
 
 const Users = require("../users/users_model");
 
-// middleware goes here
 const {
-    checkUserExists,
-    validateUsername,
-    validatePassword,
-    checkPasswordCorrect,
-    validateContact,
-    hashThePassword
-} = require('../middleware/authMiddleware')
+  checkUserExists,
+  validateUsername,
+  validatePassword,
+  checkPasswordCorrect,
+  validateContact,
+  hashThePassword,
+} = require("../middleware/authMiddleware");
 
 //[POST] /auth/register
-router.post("/register", validateUsername, validatePassword, validateContact, hashThePassword, (req, res, next) => {
-  Users.create(req.body)
-    .then((newUser) => {
-      res.status(201).json(newUser);
-    })
-    .catch(next);
-});
+router.post(
+  "/register",
+  validateUsername,
+  validatePassword,
+  validateContact,
+  hashThePassword,
+  (req, res, next) => {
+    Users.create(req.body)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch(next);
+  }
+);
 
 //[POST] /auth/login
-router.post("/login", checkUserExists, checkPasswordCorrect, (req, res, next) => {
-  try {
-    res.status(200).json({
-      user_id: req.user_id,
-      message: `Welcome back ${req.body.username}`,
-      token: req.token,
-    });
-  } catch (err) {
-    next(err);
+router.post(
+  "/login",
+  checkUserExists,
+  checkPasswordCorrect,
+  (req, res, next) => {
+    try {
+      res.status(200).json({
+        user_id: req.user_id,
+        message: `Welcome back ${req.body.username}`,
+        token: req.token,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 module.exports = router;
