a7e77cb1b1c9e75072820a705a94024a686f31c7
Christina Yun <poncheeyun@yahoo.com>
2022-01-28 13:50:14 -0800

    Finished post for have and iso

--- api/pins/pinRouter.js
+++ api/pins/pinRouter.js
@@ -14,7 +14,7 @@ const {
 const Pins = require("../pins/pins_model");
 
 //[GET] /pins/:pin_id
-router.get("/:pin_id", checkIfPinExists, (req, res, next) => {
+router.get("/:pin_id",restricted,  checkIfPinExists, (req, res, next) => {
   Pins.findById(req.params.pin_id)
     .then((pin) => {
       res.status(200).json(pin);
@@ -24,7 +24,7 @@ router.get("/:pin_id", checkIfPinExists, (req, res, next) => {
 
 //[GET] /pins/maker/:maker
 //TODO frontend needs to convert spaces to '-' before sending the request
-router.get("/maker/:maker", checkIfMakerExists, (req, res, next) => {
+router.get("/maker/:maker", restricted, checkIfMakerExists, (req, res, next) => {
   Pins.findByMaker(req.params.maker)
     .then((pins) => {
       res.status(200).json(pins);
@@ -33,7 +33,7 @@ router.get("/maker/:maker", checkIfMakerExists, (req, res, next) => {
 });
 
 //[GET] /pins/:pin_id/iso
-router.get("/:pin_id/iso", checkIfPinExists, (req, res, next) => {
+router.get("/:pin_id/iso", restricted, checkIfPinExists, (req, res, next) => {
   Pins.findUsersIsoPin(req.params.pin_id)
     .then((users) => {
       res.status(200).json(users);
@@ -42,7 +42,7 @@ router.get("/:pin_id/iso", checkIfPinExists, (req, res, next) => {
 });
 
 //[GET] /pins/:pin_id/have
-router.get("/:pin_id/have", checkIfPinExists, (req, res, next) => {
+router.get("/:pin_id/have", restricted, checkIfPinExists, (req, res, next) => {
   Pins.findUsersWhoHavePin(req.params.pin_id)
     .then((users) => {
       res.status(200).json(users);
@@ -51,11 +51,34 @@ router.get("/:pin_id/have", checkIfPinExists, (req, res, next) => {
 });
 
 //[POST] /pins/:pin_id/have
-//TODO need a way to attach pins a user has to their profile (post)
-//checkIfPinExists
+//onlyOnce
+router.post("/:pin_id/have", restricted, checkIfPinExists, (req, res, next) => {
+  const pinHave = {
+    user_id: req.decodedToken.subject,
+    pin_id: parseInt(req.params.pin_id),
+  };
+
+  Pins.addHave(pinHave)
+    .then((have) => {
+      res.status(201).json(have);
+    })
+    .catch(next);
+});
 
 //[POST] /pins/:pin_id/iso
 //checkIfPinExists
+router.post("/:pin_id/iso", restricted, checkIfPinExists, (req, res, next) => {
+  const pinIso = {
+    user_id: req.decodedToken.subject,
+    pin_id: parseInt(req.params.pin_id),
+  };
+
+  Pins.addISO(pinIso)
+    .then((iso) => {
+      res.status(201).json(iso)
+    })
+    .catch(next);
+});
 
 //[DELETE] /pins/:pin_id/have
 //TODO need a way to delete a pin from have
@@ -67,7 +90,7 @@ router.get("/:pin_id/have", checkIfPinExists, (req, res, next) => {
 
 //[POST] /pins/
 //TODO validation that all fields are correct
-router.post("/", convertForDB, noPinDupes, (req, res, next) => {
+router.post("/", restricted, convertForDB, noPinDupes, (req, res, next) => {
   Pins.createPin(req.body)
     .then((new_pin) => {
       res.status(201).json(new_pin);
@@ -77,7 +100,7 @@ router.post("/", convertForDB, noPinDupes, (req, res, next) => {
 
 //[PUT] /pins/:pin_id
 //TODO needs some kind of validation middleware for imgurl and maker
-router.put("/:pin_id", checkIfPinExists, convertForDB, (req, res, next) => {
+router.put("/:pin_id", restricted, checkIfPinExists, convertForDB, (req, res, next) => {
   Pins.updatePin(req.params.pin_id, req.body)
     .then((updated_pin) => {
       res.status(200).json(updated_pin);
@@ -87,7 +110,7 @@ router.put("/:pin_id", checkIfPinExists, convertForDB, (req, res, next) => {
 
 //[DELETE] /pins/:pin_id
 // should have admin access to delete pin from database
-router.delete("/:pin_id", checkIfPinExists, (req, res, next) => {
+router.delete("/:pin_id", restricted, checkIfPinExists, (req, res, next) => {
   Pins.removePin(req.params.pin_id)
     .then(() => {
       res.status(200).json({ message: "Pin deleted!" });
@@ -97,7 +120,7 @@ router.delete("/:pin_id", checkIfPinExists, (req, res, next) => {
 
 //TAGS stuff
 //[GET] /pins/tags/:tag_name
-router.get("/tags/:tag_name", checkIfTagExists, convertForDB, (req, res, next) => {
+router.get("/tags/:tag_name", restricted, checkIfTagExists, convertForDB, (req, res, next) => {
   Pins.findByTag(req.params.tag_name)
     .then((pins) => {
       res.status(200).json(pins);
@@ -106,7 +129,7 @@ router.get("/tags/:tag_name", checkIfTagExists, convertForDB, (req, res, next) =
 });
 
 // [GET] /pins/:pin_id/tags
-router.get("/:pin_id/tags", checkIfPinExists, (req, res, next) => {
+router.get("/:pin_id/tags", restricted, checkIfPinExists, (req, res, next) => {
   Pins.findTagsByPin(req.params.pin_id)
   .then((tags) => {
     res.status(200).json(tags)
@@ -116,7 +139,7 @@ router.get("/:pin_id/tags", checkIfPinExists, (req, res, next) => {
 
 //[POST] /pins/:pin_id/tags
 //noTagDupes
-router.post("/:pin_id/tags", convertForDB, checkIfPinExists, noTagDupes, (req, res, next) => {
+router.post("/:pin_id/tags", restricted, convertForDB, checkIfPinExists, noTagDupes, (req, res, next) => {
   req.body.pin_id = req.params.pin_id;
 
   Pins.createTag(req.body)
@@ -127,7 +150,7 @@ router.post("/:pin_id/tags", convertForDB, checkIfPinExists, noTagDupes, (req, r
 });
 
 //[DELETE] /pins/:pin_id/tags/:tag_name
-router.delete("/:pin_id/tags/:tag_name", convertForDB, checkIfPinExists, checkIfTagExists, (req, res, next) => {
+router.delete("/:pin_id/tags/:tag_name", restricted, convertForDB, checkIfPinExists, checkIfTagExists, (req, res, next) => {
   Pins.removeTag(req.params.pin_id, req.params.tag_name)
     .then(() => {
       res.status(200).json({ message: "Tag deleted!" });
